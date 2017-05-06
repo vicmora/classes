b80c94a3657ff8297304d40f12d4b32a4c9f9651
vicmora <victormora@gmail.com>
2017-05-05 22:43:49 -0700

    complete hw7

--- cnit133/js/hw7.js
+++ cnit133/js/hw7.js
@@ -46,3 +46,65 @@ $(document).ready(function () {
         $('#summary').removeClass().addClass($(this).val());
     });
 })
+
+$(document).ready(function () {
+    // Attach a click handler to the element: <p id="open">
+    $('#open').click(function () {
+        // Save the <p id="open"> and <form> elements in local variables.
+        var openElem = $(this);
+        var loginForm = $('#login form');
+
+        if (loginForm.css("display") == "none") {
+            // If the form is currently not displayed,
+            // slide it down into view, and when done add the "close" class to the
+            // <p id="open"> element.
+            loginForm.slideDown(300, function () {
+                openElem.addClass("close");
+            })
+        } else {
+            // Otherwise, fade the login form out, and when done,
+            // remove the "close" class from the <p id="open"> element.
+            loginForm.fadeOut(600, function () {
+                openElem.removeClass("close");
+            })
+        }
+    });
+}); // end ready
+
+
+function checkPassword() {
+    var frm = document.forms['passForm'];
+    var password = frm.elements['password'].value;
+
+    if(password === 'obama') {
+        window.open('part2cont.html');
+    } else {
+        document.getElementById('message').innerHTML = 'ERROR: Incorrect password (try obama)';
+    };
+}
+
+function res() {
+    document.getElementById('message').innerHTML = '';
+}
+
+$(document).ready(function() {
+    try {
+        $("#nav").navPlugin({
+        'itemWidth': 150,
+        'itemHeight': 30,
+        'navEffect': "slide",
+        'speed': 250
+        });
+    } catch(e) {
+        // handle error
+    };
+
+}); // end ready
+
+$(document).ready(function() {
+    try {
+        $('.fancybox').fancybox();
+    } catch(e) {
+        // handle error
+    };
+});
